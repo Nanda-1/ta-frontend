4fc85c9d8d3b6b6174859adc1756fc11abcd8caf
Gnabilah <g.nabilah@ib-synergy.co.id>
2022-12-07 10:42:24 +0700

    ADIFEW-18 menambahkan keterangan di semua upload foto register

---
+++ src/components/RegistPPAT/KeteranganPhoto.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const KeteranganPhoto = () => {
+  return (
+    <div className="text-coolGray-900 mx-auto pl-12 pt-2 pb-4 text-left w-auto">
+      <small>
+        Perhatian: <br />
+        1. Gambar identitas harus terbaca jelas <br />
+        2. Foto identitas adalah dokumen asli, bukan dokumen fotokopi. <br />
+        3. Identitas yang terdaftar adalah data yang masih berlaku
+      </small>
+    </div>
+  );
+};
+
+export default KeteranganPhoto;
