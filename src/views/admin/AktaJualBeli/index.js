16824724ca8983c7666e1b2fd86e3995158d1155
Gnabilah <g.nabilah@ib-synergy.co.id>
2023-04-29 22:41:01 +0700

    DIPPAT-233 create akta

--- src/views/admin/AktaJualBeli/index.js
+++ src/views/admin/AktaJualBeli/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import MasterFormAjb from "components/AJB/MasterForm";
 import { Container, Row, Col } from "reactstrap";
+import Cookies from "js-cookie";
 
 export default function stepperIndexAjb() {
+  let step = Cookies.get("step");
+
   return (
     <Container className="mx-auto">
       <Row>
-        <Col className="pt-8 mx-auto lg:w-8/12">
-          <MasterFormAjb className='h-full'/>
+        <Col
+          className={`pt-8 mx-auto ${
+            step === "dokumen" ? "lg:w-12/12" : "lg:w-8/12"
+          }`}
+        >
+          <MasterFormAjb className="h-full" />
         </Col>
       </Row>
     </Container>
