60e15729209221882005e95be73890a01006f9b6
Gnabilah <g.nabilah@ib-synergy.co.id>
2023-04-29 22:25:46 +0700

    DIPPAT-272 show all payment moethod

---
+++ src/components/Payment/OtherPayment.js
@@ -0,0 +1,87 @@
+import React, { useContext } from "react";
+import { TopUpContext } from "Context/TopUpContext";
+import Bri from "assets/img/icon/bank_bri.png";
+import Bni from "assets/img/icon/bank_bni.png";
+import Permata from "assets/img/icon/bank_permata.png";
+
+export default function OtherPayment() {
+  const { setListPayment, listPayment, setOtherPayment } =
+    useContext(TopUpContext);
+
+  // const { topUpPay } = functions;
+
+  const handleButton = (type) => {
+    let text1 = "payment_type";
+    setListPayment({ [text1]: type });
+  };
+
+  // if (listPayment.length !== 0) {
+  //   topUpPay(checkout.top_up_transaction_id, "");
+  // }
+  return (
+    <div className="font-roboto z-50 font-bold text-black ">
+      <div className="flex mb-2">
+        <button
+          className="focus:outline-none pr-3 mt-1"
+          type="button"
+          onClick={() => setOtherPayment(false)}
+        >
+          <i className="fa fa-times fa-lg"></i>
+        </button>
+        <h4 className="w-full font-700 py-1 text-black">Pilih Pembayaran</h4>
+      </div>
+      <span className="font-bold text-sm">Transfer Virtual Account</span>
+      <button
+        className="cursor-pointer flex items-center border-b-2 text-sm text-left w-full p-3 my-1 outline-none focus:outline-none"
+        onClick={() => handleButton("bni")}
+      >
+        <img src={Bni} className="bank-icon mr-4" alt="Bni" />
+        <div className="flex flex-col mr-auto">
+          <span className="font-bold">BNI</span>
+          <label className="text-xxs text-grey">Biaya transaksi Rp4.000</label>
+        </div>
+        <input
+          type="radio"
+          className="radio-payment my-1"
+          checked={listPayment.payment_type === "bni"}
+        />
+      </button>
+      <button
+        className="cursor-pointer flex items-center border-b-2 text-sm text-left w-full p-3 my-1 outline-none focus:outline-none"
+        onClick={() => handleButton("permata")}
+      >
+        <img src={Permata} className="bank-icon mr-4" alt="Permata" />
+        <div className="flex flex-col mr-auto">
+          <span className="font-bold">Permata</span>
+          <label className="text-xxs text-grey">Biaya transaksi Rp4.000</label>
+        </div>
+        <input
+          type="radio"
+          className="radio-payment my-1"
+          checked={listPayment.payment_type === "permata"}
+        />
+      </button>
+      <button
+        className="cursor-pointer flex items-center border-b-2 text-sm text-left w-full p-3 my-1 outline-none focus:outline-none"
+        onClick={() => handleButton("bri")}
+      >
+        <img src={Bri} className="bank-icon mr-4" alt="Bri" />
+        <div className="flex flex-col mr-auto">
+          <span className="font-bold">BRI</span>
+          <label className="text-xxs text-grey">Biaya transaksi Rp4.000</label>
+        </div>
+        <input
+          type="radio"
+          className="radio-payment my-1"
+          checked={listPayment.payment_type === "bri"}
+        />
+      </button>
+
+      <div className="mt-6 border-t">
+        <button className="bg-blue my-4 py-2 w-full text-white rounded-md">
+          Pilih Metode Pembayaran
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/components/Payment/PaymentList.js
+++ src/components/Payment/PaymentList.js
@@ -1,70 +1,73 @@
 import React, { useContext } from "react";
 import { TopUpContext } from "Context/TopUpContext";
-import Bri from "assets/img/icon/bank_bri.png";
-import Bni from "assets/img/icon/bank_bni.png";
 import Bca from "assets/img/icon/bank_bca.png";
 import Mandiri from "assets/img/icon/bank_mandiri.png";
-import Permata from "assets/img/icon/bank_permata.png";
 import Gopay from "assets/img/icon/gopay_.png";
 
 export default function PaymentList() {
-  const { setListPayment, listPayment, checkout, functions } =
-    useContext(TopUpContext);
-
-  const { topUpPay } = functions;
+  const { setListPayment, listPayment } = useContext(TopUpContext);
 
   const handleButton = (type) => {
     let text1 = "payment_type";
     setListPayment({ [text1]: type });
   };
 
-  if (listPayment.length !== 0) {
-    topUpPay(checkout.top_up_transaction_id, "");
-  }
+  // if (listPayment.length !== 0) {
+  //   topUpPay(checkout.top_up_transaction_id, "");
+  // }
   return (
     <div className="font-roboto font-bold text-black">
       <>
         <button
-          className="border cursor-pointer hover:pilihPayment text-sm text-left w-full p-3 my-1 rounded-md"
+          className="cursor-pointer flex items-center border-b-2 text-sm text-left w-full p-3 my-1 outline-none focus:outline-none"
           onClick={() => handleButton("mandiri")}
         >
-          <img src={Mandiri} className="bank-icon" alt="Mandiri" />
-          <span className="font-bold p-2">Bank Mandiri</span>
+          <img src={Mandiri} className="bank-icon mr-4" alt="Mandiri" />
+          <div className="flex flex-col mr-auto">
+            <span className="font-bold">Mandiri Virtual Account</span>
+            <label className="text-xxs text-grey">
+              Biaya transaksi Rp4.000
+            </label>
+          </div>
+          <input
+            type="radio"
+            className="radio-payment my-1"
+            checked={listPayment.payment_type === "mandiri"}
+          />
         </button>
         <button
-          className="border cursor-pointer hover:pilihPayment text-sm text-left w-full p-3 my-1 rounded-md"
-          onClick={() => handleButton("bni")}
-        >
-          <img src={Bni} className="bank-icon" alt="Bni" />
-          <span className="font-bold p-2">Bank BNI</span>
-        </button>
-        <button
-          className="border cursor-pointer hover:pilihPayment text-sm text-left w-full p-3 my-1 rounded-md"
-          onClick={() => handleButton("permata")}
-        >
-          <img src={Permata} className="bank-icon" alt="Permata" />
-          <span className="font-bold p-2">Permata Bank</span>
-        </button>
-        <button
-          className="border cursor-pointer hover:pilihPayment text-sm text-left w-full p-3 my-1 rounded-md"
+          className="cursor-pointer flex items-center border-b-2 text-sm text-left w-full p-3 my-1 outline-none focus:outline-none"
           onClick={() => handleButton("bca")}
         >
-          <img src={Bca} className="bank-icon" alt="Bca" />
-          <span className="font-bold p-2">Bank BCA</span>
+          <img src={Bca} className="bank-icon mr-4" alt="Bca" />
+          <div className="flex flex-col mr-auto">
+            <span className="font-bold">Bank BCA</span>
+            <label className="text-xxs text-grey">
+              Biaya transaksi Rp4.000
+            </label>
+          </div>
+          <input
+            type="radio"
+            className="radio-payment my-1"
+            checked={listPayment.payment_type === "bca"}
+          />
         </button>
         <button
-          className="border cursor-pointer hover:pilihPayment text-sm text-left w-full p-3 my-1 rounded-md"
-          onClick={() => handleButton("bri")}
-        >
-          <img src={Bri} className="bank-icon" alt="Bri" />
-          <span className="font-bold p-2">Bank BRI</span>
-        </button>
-        <button
-          className="border cursor-pointer hover:pilihPayment text-sm text-left w-full p-3 my-1 rounded-md"
+          className="cursor-pointer flex items-center text-sm text-left w-full p-3 my-1 outline-none focus:outline-none"
           onClick={() => handleButton("gopay")}
         >
-          <img src={Gopay} className="bank-icon" alt="Gopay" />
-          <span className="font-bold p-2 text-md">Gopay</span>
+          <img src={Gopay} className="bank-icon mr-4" alt="Gopay" />
+          <div className="flex flex-col mr-auto">
+            <span className="font-bold text-md">Gopay</span>
+            <label className="text-xxs text-grey">
+              Biaya transaksi 2% dari total pembelian
+            </label>
+          </div>
+          <input
+            type="radio"
+            className="radio-payment my-1"
+            checked={listPayment.payment_type === "gopay"}
+          />
         </button>
       </>
     </div>
