76103052ccc7e5ac1929889ed05a96839379a618
Gnabilah <g.nabilah@ib-synergy.co.id>
2023-04-29 22:08:02 +0700

    DIPPAT-164 create webRTC module for show document and create virtual room

--- src/components/Agora/AgoraRtc.js
+++ src/components/Agora/AgoraRtc.js
@@ -1,48 +1,49 @@
-import React, { useState } from "react";
-import Cookies from "js-cookie";
-
-const appId = "15ade710d3de457bbd2ddc96f487c621"; //ENTER APP ID HERE7eJxTYPjPIsG6PXX/ptlCk40LKt7nFy71Y4j7ZO5yaEHxfedXTyUUGAxNE1NSzQ0NUoxTUk1MzZOSUoxSUpItzdJMLMyTzYwMv8+sTW4IZGToYytmZWSAQBCfnaEktbgkMy+dgQEAsIUg/g==";
+import React, { useContext, useEffect } from "react";
+import { MyAjbContext } from "Context/AjbContext";
+import DocumentReady from "./DocumentReady";
+import AgoraVideoCall from "./AgoraVideoCall";
 
 const AgoraRtc = () => {
-  const [inCall, setInCall] = useState(false);
-  const [channelName, setChannelName] = useState("");
-  if (inCall) {
-    Cookies.set("channelName", channelName);
-    window.location.reload();
-  }
-  return (
-    <div>
-      <h1 className="heading" style={{ marginTop: "20px" }}>
-        Agora RTC NG SDK React Wrapper
-      </h1>
-      <ChannelForm setInCall={setInCall} setChannelName={setChannelName} />
-    </div>
-  );
-};
+  const {
+    functions,
+    setLoadingFile,
+    ajbDoc,
+    ttdImage,
+    setBtnConfirm,
+    btnConfirm,
+    inputAjb,
+    setInputAjb,
+    loadingFile,
+    otpModal
+  } = useContext(MyAjbContext);
 
-const ChannelForm = (props) => {
-  const { setInCall, setChannelName } = props;
+  const { getDokumenAjb, getTtdImage, addTandaTangan } = functions;
+
+  useEffect(() => {
+    getDokumenAjb();
+    getTtdImage();
+    setLoadingFile(true);
+  }, []);
+
   return (
-    <form className="join">
-      {appId === "" && (
-        <p style={{ color: "red" }}>
-          Please enter your Agora App ID in App.tsx and refresh the page
-        </p>
-      )}
-      <input
-        type="text"
-        placeholder="Enter Channel Name"
-        onChange={(e) => setChannelName(e.target.value)}
-      />
-      <button
-        onClick={(e) => {
-          e.preventDefault();
-          setInCall(true);
-        }}
-      >
-        Join
-      </button>
-    </form>
+    <div className="Example__container_pdf text-sm w-full h-screen">
+      <div className="flex w-full py-4" style={{ overflow: "hidden" }}>
+        {/* <div className="grid grid-cols-3 p-4"> */}
+        <DocumentReady
+          ajbDoc={ajbDoc}
+          ttdImage={ttdImage}
+          setBtnConfirm={setBtnConfirm}
+          btnConfirm={btnConfirm}
+          inputAjb={inputAjb}
+          setInputAjb={setInputAjb}
+          addTandaTangan={addTandaTangan}
+          loadingFile={loadingFile}
+          setLoadingFile={setLoadingFile}
+          otpModal={otpModal}
+        />
+        <AgoraVideoCall />
+      </div>
+    </div>
   );
 };
 
